1099af8e73866a7e3417c566bf43993b03283416
Gianluigi Trontini <gianluigitrontini@gmail.com>
2022-01-10 09:50:51 +0100

    Initialize Project

---
+++ src/components/global/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div></div>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
